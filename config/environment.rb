d53aaba606e883ab4336cc46b4df443b2dc908b4
Junior <ycetrey@gmail.com>
2017-05-22 13:37:03 -0300

    Define enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# force Rails into production mode when                          
+# you don't control web/app server and can't set it the proper way                  
+ENV['RAILS_ENV'] ||= 'production'

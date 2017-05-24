dc2777948acff0a7d32909f8da02f8a0523f82a5
Junior <ycetrey@gmail.com>
2017-05-24 10:37:53 -0300

    Remove ignored files

--- db/migrate/20170522140452_add_file_to_abanda.rb
+++
@@ -1,5 +0,0 @@
-class AddFileToAbanda < ActiveRecord::Migration[5.1]
-  def change
-    add_column :abandas, :file, :string
-  end
-end
--- db/migrate/20170523180057_add_field_file_to_photo.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldFileToPhoto < ActiveRecord::Migration[5.1]
-  def change
-    add_column :photos, :file, :string
-  end
-end

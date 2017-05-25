757f4fbcc09b24c4ec0b209987cbef1a3c15588d
Junior <ycetrey@gmail.com>
2017-05-25 17:11:02 -0300

    Adicionado Patrocinio

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git add .
 git commit -am "$1"
+git push origin master
 echo "Git atualizado, executando rsync..."
 rsync -azP ./* root@67.205.191.138:/home/rails/canoserrado/
 ssh root@67.205.191.138 "chown -R rails: /home/rails/canoserrado"

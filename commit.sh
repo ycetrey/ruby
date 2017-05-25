1be799c0292047a311ea913642bcac958d9234ee
Junior <ycetrey@gmail.com>
2017-05-25 11:47:53 -0300

    Alteracoes testando commit

--- commit.sh
+++ commit.sh
@@ -3,4 +3,4 @@ git add .
 git commit -am "$1"
 echo "Git atualizado, executando rsync..."
 rsync -azP ./* root@67.205.191.138:/home/rails/canoserrado/
-ssh root@67.205.191.138 "chown -R rails:/home/rails/canoserrado"
+ssh root@67.205.191.138 "chown -R rails: /home/rails/canoserrado"

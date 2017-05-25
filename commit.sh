ab0a0fc4929a6cb9e9562345888f3fc6f8d48e73
Junior <ycetrey@gmail.com>
2017-05-25 17:14:36 -0300

    Remove ignored files

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add .
-git commit -am "$1"
-git push origin master
-echo "Git atualizado, executando rsync..."
-rsync -azP ./* root@67.205.191.138:/home/rails/canoserrado/
-ssh root@67.205.191.138 "chown -R rails: /home/rails/canoserrado"
-ssh root@67.205.191.138 "cd /home/rails/canoserrado && bundle"
-ssh root@67.205.191.138 "service unicorn restart && service nginx restart"
